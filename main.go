c2fa148eaa728ca17e3ee704635abd661f8d0760
Maxim Yefremov <maxYefr@gmail.com>
2022-02-09 07:48:55 +0300

    attempts

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 	get(&result) // I expect that result equals "1" after calling "get" function but it's "0"
 	log.Println(result) // it prints "0" here but I would like to fix code in way so it should print "1" here
 	// set function should accept arbitrary type
+
+	// I can do the following though:
+	resultI := get2()
+	result = resultI.(int)
+	log.Println("result", result)
+	// but is it possible to encapsulate the line
+	// result = resultI.(int)
+	// inside method "get"
+
 }
 
 var (
@@ -31,3 +40,30 @@ func get(c interface{}) {
 	c = a
 }
 
+func get2() interface{} {
+	aMtx.Lock()
+	defer aMtx.Unlock()
+	return a
+}
+
+func get3(c interface{}) {
+	aMtx.Lock()
+	defer aMtx.Unlock()
+//	t := reflect.TypeOf(c)
+//	c = a.(t) // looks like it's impossible https://stackoverflow.com/q/27971895/1024794
+}
+
+func get4(c interface{}) {
+	aMtx.Lock()
+	defer aMtx.Unlock()
+	//json.Unmarshal() // kind of this?
+}
+
+func get5(c interface{}) {
+	aMtx.Lock()
+	defer aMtx.Unlock()
+	//switch v := c.(type) {
+	//case int:
+	//*c = v
+	//}
+}
