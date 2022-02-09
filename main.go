545b843f3a4a8c2f9747f6f747a3ee0ac0a91384
Maxim Yefremov <maxYefr@gmail.com>
2022-02-09 07:18:36 +0300

    mtx

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sync"
+)
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Llongfile)
@@ -8,18 +11,23 @@ func main() {
 	var result int
 	get(&result) // I expect that result equals "1" after calling "get" function but it's "0"
 	log.Println(result) // it prints "0" here but I would like to fix code in way so it should print "1" here
+	// set function should accept arbitrary type
 }
 
 var (
 	a interface{}
-
+	aMtx sync.Mutex
 )
 
 func set(b interface{}) {
+	aMtx.Lock()
+	defer aMtx.Unlock()
 	a = b
 }
 
 func get(c interface{}) {
+	aMtx.Lock()
+	defer aMtx.Unlock()
 	c = a
 }
 
